d5dfbdf39daa240fb98290d9840402511bc92af4
Christi <ccapati@terpmail.umd.edu>
2015-10-09 11:39:08 +0530

    pojos created for Order, Blocks, and Executred Blocks

---
+++ src/main/java/com/java/pojo/Block.java
@@ -0,0 +1,109 @@
+package com.java.pojo;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+
+/**
+ * The persistent class for the blocks database table.
+ * 
+ */
+@Entity
+@Table(name="blocks")
+@NamedQuery(name="Block.findAll", query="SELECT b FROM Block b")
+public class Block implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="block_id")
+	private String blockId;
+
+	@Column(name="executed_qty")
+	private int executedQty;
+
+	@Column(name="limit_price")
+	private float limitPrice;
+
+	@Column(name="open_qty")
+	private int openQty;
+
+	private String status;
+
+	@Column(name="stop_price")
+	private float stopPrice;
+
+	private Timestamp timestamp;
+
+	@Column(name="total_qty")
+	private int totalQty;
+
+	public Block() {
+	}
+
+	public String getBlockId() {
+		return this.blockId;
+	}
+
+	public void setBlockId(String blockId) {
+		this.blockId = blockId;
+	}
+
+	public int getExecutedQty() {
+		return this.executedQty;
+	}
+
+	public void setExecutedQty(int executedQty) {
+		this.executedQty = executedQty;
+	}
+
+	public float getLimitPrice() {
+		return this.limitPrice;
+	}
+
+	public void setLimitPrice(float limitPrice) {
+		this.limitPrice = limitPrice;
+	}
+
+	public int getOpenQty() {
+		return this.openQty;
+	}
+
+	public void setOpenQty(int openQty) {
+		this.openQty = openQty;
+	}
+
+	public String getStatus() {
+		return this.status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public float getStopPrice() {
+		return this.stopPrice;
+	}
+
+	public void setStopPrice(float stopPrice) {
+		this.stopPrice = stopPrice;
+	}
+
+	public Timestamp getTimestamp() {
+		return this.timestamp;
+	}
+
+	public void setTimestamp(Timestamp timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public int getTotalQty() {
+		return this.totalQty;
+	}
+
+	public void setTotalQty(int totalQty) {
+		this.totalQty = totalQty;
+	}
+
+}
